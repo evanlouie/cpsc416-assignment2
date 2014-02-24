0c80db4e67baa497585579de401f10ed9391e435
Evan Louie <louie.evan@gmail.com>
2014-02-23 16:26:38 -0800

    Initial

--- cpsc416-assignment2/main.c
+++ cpsc416-assignment2/main.c
@@ -7,12 +7,29 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <time.h>
+
 
 int main(int argc, const char * argv[])
 {
 
     // insert code here...
-    printf("Hello, World!\n");
+    printf("%s\n", argv[1]);
+    for (int i=0; i<=argc; i++) {
+        printf("%d: %s\n", i, argv[i]);
+    }
+    
+    FILE * pFile;
+    pFile = fopen ("result.txt","w+");
+    if (pFile!=NULL)
+    {
+        fputs ("fopen example asdf\n",pFile);
+        fclose (pFile);
+    }
+
     return 0;
 }
 
